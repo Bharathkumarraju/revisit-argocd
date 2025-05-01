ebe2a6c30c2d426250b6da9bf37062628ad781d7
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-05-01 21:01:45 +0800

    Apps of apps pattern

---
+++ 4.argocd-progressive-delivery/1.demo.sh
@@ -0,0 +1,7 @@
+Use argorollouts for
+1. Blue/Green Deployment  -- https://github.com/codefresh-contrib/gitops-certification-examples/tree/main/blue-green-app
+2. Canary Deployment  -- https://github.com/codefresh-contrib/gitops-certification-examples/tree/main/canary-app-manual and https://github.com/codefresh-contrib/gitops-certification-examples/tree/main/canary-app-automatic
+3. automatic rollback using merics
+
+there is rollout CRD
+
