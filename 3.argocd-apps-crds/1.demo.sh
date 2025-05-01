80c3ea8324182d9e1223b7c86caeb8b28a323e23
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-05-01 17:39:48 +0800

    deploy argocd apps crds'

---
+++ 3.argocd-apps-crds/1.demo.sh
@@ -0,0 +1,9 @@
+For example when you create an application in Argo CD, essentially you are creating a grouping between:
+
+  1.A Git repository that holds your manifest
+  2.An Argo CD project
+  3. A cluster
+  4. Several other custom settings (extra parameters, target namespace etc.)
+
+
+
