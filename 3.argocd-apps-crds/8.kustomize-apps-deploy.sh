55b5403ef16e043675f2f5de909461082278d7f7
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-05-01 19:45:08 +0800

    introduce argo rollouts controller

--- 3.argocd-apps-crds/8.kustomize-apps-deploy.sh
+++ 3.argocd-apps-crds/8.kustomize-apps-deploy.sh
@@ -8,3 +8,14 @@ root@host:~/workdir# argocd app create staging-app \
 application 'staging-app' created
 root@host:~/workdir#
 
+
+
+root@host:~/workdir# argocd app create production-app \
+--project default \
+--repo https://github.com/Bharathkumarraju/gitops-certification-examples/ \
+--path "./kustomize-apps/overlays/production" \
+--sync-policy auto \
+--dest-namespace default \
+--dest-server https://kubernetes.default.svc
+application 'production-app' created
+root@host:~/workdir#
